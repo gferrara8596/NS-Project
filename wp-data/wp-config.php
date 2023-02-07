1e69a644db7878a085c662c695428d5e03b13497
Mauro Galateo <mauro.galateo.996@gmail.com>
2023-02-07 16:57:37 +0100

    aggiunte vulnerabilità e sondaggio

--- wp-data/wp-config.php
+++ wp-data/wp-config.php
@@ -46,15 +46,15 @@ define( 'DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '39e5fe83332e0636a3ca0313c6bc1503b8629058');
-define( 'SECURE_AUTH_KEY',  '11d82c6ffe8396b099819fe840c3fbf75c6c785c');
-define( 'LOGGED_IN_KEY',    '0c71cdcb58b0adcfca34689c4669ee32e310e384');
-define( 'NONCE_KEY',        'd618e3566cb34d726c417d1feac32a8062df864e');
-define( 'AUTH_SALT',        '429d0908335c5c952004534e16ea40e48496da25');
-define( 'SECURE_AUTH_SALT', '71c9825d66f8b4e4d8454c6e3dff66940aa905f5');
-define( 'LOGGED_IN_SALT',   'c6d43044cfd3f00f1557d27b2445aaa97a539e21');
-define( 'NONCE_SALT',       'b0461e36ee4b001514869b26731f11ec993b43f0');
-define( 'WP_AUTO_UPDATE_CORE', false);
+define( 'AUTH_KEY',         '687eff23ce7b919b2c66dacb606e99069c1cabe5');
+define( 'SECURE_AUTH_KEY',  'c90827720240046da39e098af42c0f316d879aae');
+define( 'LOGGED_IN_KEY',    'ef40018844802c57d0b1f33d3a9d43b05800955f');
+define( 'NONCE_KEY',        'd57af51688a835f4389585d82d174f5595aacac5');
+define( 'AUTH_SALT',        '7f7dc16cf470e1a0e685542367a7c144a08fe279');
+define( 'SECURE_AUTH_SALT', '44fdab4803b0bd3386ca5c348678a3377055d733');
+define( 'LOGGED_IN_SALT',   '03cac27ade9554ea5f0037578c742c8736267790');
+define( 'NONCE_SALT',       '80277ff138119a328d6825980ab1865164a33ebc');
+
 /**#@-*/
 
 /**
